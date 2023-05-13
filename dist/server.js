7791eeb9c3524821f90aa90882bc5cabf2dfc5fc
Maximilian <scientificresearch309@gmail.com>
2023-05-14 00:16:38 +0200

    __dirname works!

--- dist/server.js
+++ dist/server.js
@@ -1,20 +1,24 @@
 // const message: string = "typescript works.";
 // console.log(message);
 import express from "express";
+import path from "path";
 const app = express();
+const __dirname = path.resolve(path.dirname(""));
 const port = 3944;
-app.get("/", (req, res) => {
-    res.send(`
-  <h1>Home</h1>
-  <p>go to <a href="info">info page</a></p>
-  `);
-});
-app.get("/info", (req, res) => {
-    res.send(`
-    <h1>Info</h1>
-    <p>go back <a href="/">home</a></p>
-    `);
-});
+// console.log(__dirname);
+app.use(express.static(__dirname + "/public"));
+// app.get("/", (req: express.Request, res: express.Response) => {
+//   res.send(`
+//   <h1>Home</h1>
+//   <p>go to <a href="info">info page</a></p>
+//   `);
+// });
+// app.get("/info", (req: express.Request, res: express.Response) => {
+//   res.send(`
+//     <h1>Info</h1>
+//     <p>go back <a href="/">home</a></p>
+//     `);
+// });
 app.listen(port, () => {
     console.log(`now listening on http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -5,22 +5,25 @@ import express from "express";
 import path from "path";
 
 const app = express();
-
+const __dirname = path.resolve(path.dirname(""));
 const port = 3944;
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send(`
-  <h1>Home</h1>
-  <p>go to <a href="info">info page</a></p>
-  `);
-});
+// console.log(__dirname);
+app.use(express.static(__dirname + "/public"));
 
-app.get("/info", (req: express.Request, res: express.Response) => {
-  res.send(`
-    <h1>Info</h1>
-    <p>go back <a href="/">home</a></p>
-    `);
-});
+// app.get("/", (req: express.Request, res: express.Response) => {
+//   res.send(`
+//   <h1>Home</h1>
+//   <p>go to <a href="info">info page</a></p>
+//   `);
+// });
+
+// app.get("/info", (req: express.Request, res: express.Response) => {
+//   res.send(`
+//     <h1>Info</h1>
+//     <p>go back <a href="/">home</a></p>
+//     `);
+// });
 
 app.listen(port, () => {
   console.log(`now listening on http://localhost:${port}`);
