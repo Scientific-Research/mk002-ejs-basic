57931d7b32e31c2f6edda2ba6ab75513b2079f2b
Maximilian <scientificresearch309@gmail.com>
2023-05-13 23:17:54 +0200

    home and info pages are working together using both links!

--- dist/server.js
+++ dist/server.js
@@ -4,7 +4,16 @@ import express from "express";
 const app = express();
 const port = 3944;
 app.get("/", (req, res) => {
-    res.send(`this is <b>HTML<b>`);
+    res.send(`
+  <h1>Home</h1>
+  <p>go to <a href="info">info page</a></p>
+  `);
+});
+app.get("/info", (req, res) => {
+    res.send(`
+    <h1>Info</h1>
+    <p>go back <a href="/">home</a></p>
+    `);
 });
 app.listen(port, () => {
     console.log(`now listening on http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,17 @@ const app = express();
 const port = 3944;
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  res.send(`this is <b>HTML<b>`);
+  res.send(`
+  <h1>Home</h1>
+  <p>go to <a href="info">info page</a></p>
+  `);
+});
+
+app.get("/info", (req: express.Request, res: express.Response) => {
+  res.send(`
+    <h1>Info</h1>
+    <p>go back <a href="/">home</a></p>
+    `);
 });
 
 app.listen(port, () => {
