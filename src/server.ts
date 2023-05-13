e45aa3b3817e05a39611588e7c2aa56b09da798e
Maximilian <scientificresearch309@gmail.com>
2023-05-13 23:54:05 +0200

    path installed

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@
 // console.log(message);
 
 import express from "express";
+import path from "path";
 
 const app = express();
+
 const port = 3944;
 
 app.get("/", (req: express.Request, res: express.Response) => {
